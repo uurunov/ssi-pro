a0a4ec3221c98e987a9d715939f2d81050acb3f9
uurunov <ulugbekurunov1997@mail.ru>
2022-07-17 16:43:24 +0500

    improved sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 const sidebarToggle = document.body.querySelector("#sidebarToggle");
-const myModal = new bootstrap.Modal(document.getElementById("loadStateModal"));
+//const myModal = new bootstrap.Modal(document.getElementById("loadStateModal"));
 const loader = document.querySelector("#loadingIndicator");
-myModal.show();
 
 // Toggle the side navigation
 if (sidebarToggle) {
   //Uncomment Below to persist sidebar toggle between refreshes
-  if (localStorage.getItem("sb|sidebar-toggle") === "true") {
-    document.body.classList.toggle("sb-sidenav-toggled");
-  }
+  // if (localStorage.getItem("sb|sidebar-toggle") === "true") {
+  //   document.body.classList.toggle("sb-sidenav-toggled");
+  // }
   sidebarToggle.addEventListener("click", (event) => {
     //event.preventDefault();
     document.body.classList.toggle("sb-sidenav-toggled");
@@ -19,6 +18,16 @@ if (sidebarToggle) {
   });
 }
 
-window.addEventListener("load", (event) => {
-  myModal.hide();
-});
+// document.addEventListener("readystatechange", (event) => {
+//   if (event.target.readyState === "loading") {
+//     myModal.show();
+//   } else if (event.target.readyState === "interactive") {
+//     myModal.show();
+//   } else {
+//     myModal.hide();
+//   }
+// });
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   myModal.hide();
+// });
